d899710552d74afc9efa438d8da27f1e1eaea899
seshagiri96 <seshu.nine6@gmail.com>
2016-04-20 17:04:12 +0530

    init changes

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "6p4czGbFFtAge2ZFfQhNo7WnF"
+consumer_secret = "8B1yUzwMSI5Io4fZuErT8kdBEVIG095DHdYLWuze9ms8XADEMN"
+access_key = "2756158507-IiKnrdYJ504bWu3ieoiMLgfXspog7cmE6UAK2a6"
+access_secret = "JQ36vQMPKDjzr28YDsMcSvcrKd0ZZB1Z5ov7NCo00JN7O"
